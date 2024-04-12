0175bcdabac98cb7b9d05f9a2c24704fa7aadf6e
shinht97 <dorim123@gmail.com>
2024-04-12 14:03:42 +0900

    sound.py fix

--- cp_and_mv_ver1.0/sound.py
+++ cp_and_mv_ver1.0/sound.py
@@ -10,17 +10,17 @@ class sound_reco():
         #model = whisper.load_model("base", "cpu") # tiny/base/small/medium/large
         # Initialize recognizer class (for recognizing the speech)
         self.recognizer = sr.Recognizer()
-        
-        with sr.Microphone() as source:
-            print("Please wait. Calibrating microphone for 1 second!")
-            # listen for 1 second and create the ambient noise energy level
-            self.recognizer.adjust_for_ambient_noise(source, duration=1)
+                    
 
     def tts(self, queue):
         # Set up the microphone
         with sr.Microphone() as source:
-            print("Microphone is now listening for input up to 10 seconds")
-            audio = self.recognizer.listen(source, phrase_time_limit=10)
+            # print("Please wait. Calibrating microphone for 1 second!")
+            # listen for 1 second and create the ambient noise energy level
+            self.recognizer.adjust_for_ambient_noise(source, duration=1)
+            
+            # print("Microphone is now listening for input up to 10 seconds")
+            audio = self.recognizer.listen(source, phrase_time_limit=5)
             # Write audio to a WAV file
             with open("rec_input.wav", "wb") as f:
                 f.write(audio.get_wav_data())
@@ -29,25 +29,26 @@ class sound_reco():
         audio = whisper.load_audio("rec_input.wav")
         audio = whisper.pad_or_trim(audio)
         # make log-Mel spectrogram and move to the same device as the model
-        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+        mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
 
         # decode the audio
         options = whisper.DecodingOptions(language='ko')
-        result = whisper.decode(model, mel, options)
+        result = whisper.decode(self.model, mel, options)
 
         # print the recognized text
         print(result.text)
         
-        queue.put({"flag" : result.text})
+        queue.put({"sound" : result.text})
+        
+        return result.text
         
         
 if __name__ == "__main__":
-    sound_reco = sound_reco()
-    while True:
-        key = input()
-        if key == "q":
+    sound_rec = sound_reco()
+    while True:        
+        result = sound_rec.tts()
+        if "종료" in result.split():
             break
-        
-        sound_reco.tts()
-        
+        else:
+            continue       
         
